c2086d7464d1d22759c3e2b187c4b3d06d528428
Timm Schoenborn <timmschoenborn@gmail.com>
2024-02-02 10:58:43 -0500

    updates BurgerPane to be a function component

--- src/BurgerPane.js
+++ src/BurgerPane.js
@@ -2,33 +2,32 @@
 // each iteration of the loop will display one clickable component
 // this clickable item will be rendered by another component, called Ingredient
 
-import React, { Component } from 'react'
 import Ingredient from './Ingredient'
 
-export default class BurgerPane extends Component {
-    render () {
-        const { ingredients } = this.props
-        // console.log('the props in ing list', ingredients)
+const BurgerPane = (props) => {
+    const { ingredients } = props
+    // console.log('the props in ing list', ingredients)
 
-        let burgerBits = ingredients.map((ing, i) => (
-            // this map will immediately return one item per loop iteration
-            <li key={i}>
-                <Ingredient 
-                    ingredient={ing}
-                    clickFunc={this.props.remove}
-                    itemKey={i}
-                />
-            </li>
-        ))
+    let burgerBits = ingredients.map((ing, i) => (
+        // this map will immediately return one item per loop iteration
+        <li key={i}>
+            <Ingredient 
+                ingredient={ing}
+                clickFunc={props.remove}
+                itemKey={i}
+            />
+        </li>
+    ))
 
-        return (
-            <section className='pane'>
-                <h3>Burger Stack</h3>
-                <ul>
-                    { burgerBits }
-                </ul>
-                <button onClick={this.props.clear}>Clear Burger</button>
-            </section>
-        )
-    }
+    return (
+        <section className='pane'>
+            <h3>Burger Stack</h3>
+            <ul>
+                { burgerBits }
+            </ul>
+            <button onClick={props.clear}>Clear Burger</button>
+        </section>
+    )
 }
+
+export default BurgerPane
